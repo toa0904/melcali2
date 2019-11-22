68ec216790807d467129f98e08d8835e197dface
toa0904 <pazutomo0428@icloud.com>
2019-11-22 15:47:47 +0900

    変更

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
   get   'posts/edit6' =>  'posts#edit6'
   get   'posts/logout' => 'posts#logout'
   get   'posts/box-title' => 'posts#box-title'
+  get   "posts/info" => "posts#info"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
